5885656f0ab5849931c613f341d477823a3b4b5c
Hyunsub Kim <embrapers263@gmail.com>
2020-02-20 10:37:34 +0000

    Changed user authority default to 1

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -14,7 +14,7 @@ export class User extends BaseEntity {
   @Column()
   password: string;
 
-  @Column({ default: 0 })
+  @Column({ default: 1 })
   authority: number;
 
   @Column({ default: () => 'CURRENT_TIMESTAMP' })
